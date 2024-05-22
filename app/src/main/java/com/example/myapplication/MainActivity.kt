6f44c7efdb5ebe90b4790d0a8376039b45b946eb
Orkhan Azm <orkhanazm@Orkhans-MacBook-Air.local>
2024-05-22 20:47:23 +0400

    my app

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+                    var value by remember {
+                        mutableStateOf(0)
+                    }
+
+                    CustomComponent(
+                        indicatorValue = value
+                    )
+
+                    TextField(
+                        value = value.toString(),
+                        onValueChange = {
+                        value = if (it.isNotEmpty()) {
+                            it.toInt()
+                        }else{
+                            0
+                        } },
+                       keyboardOptions = KeyboardOptions(
+                           keyboardType = KeyboardType.Number
+                       )
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
+}
